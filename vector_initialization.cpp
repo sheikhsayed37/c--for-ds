9069869161f9672d83a411bf7e3b35071117810c
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-14 23:52:07 +0600

    3

---
+++ vector_initialization.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+ vector<int >v;
+ cout<<v.size()<<endl;
+  vector<int >v1(5);
+  cout<<v1.size()<<endl;
+   vector<int >v2(5,10);
+ for(int i=0;i<v2.size();i++)
+ {
+    cout<<v2[i]<<"  ";
+ }
+ cout<<endl;
+vector<int>v3(v2);
+for(int i=0;i<v3.size();i++)
+{
+    cout<<v3[i]<<"  ";
+}
+ cout<<endl;
+vector<int>v4{1,2,3,4,5};
+for(int i=0;i<v4.size();i++)
+{
+    cout<<v4[i]<<"  ";
+}
+}
