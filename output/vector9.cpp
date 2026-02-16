e5c83f674d407b6679eafa7ceda01e6ca8a2d938
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-16 19:38:19 +0600

    9

---
+++ output/vector9.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    
+    vector<int>v={ 1,2,3,4,5,6,8};
+    cout<<v[5]<<endl;
+    cout<<v.front()<<endl;
+    cout<<v.back()<<endl;
+
+}
