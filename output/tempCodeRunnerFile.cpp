0a04d7374240c483ea5225a1148d7cc54b56563a
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-02-16 21:56:21 +0600

    11

---
+++ output/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int >v1={1,2,3,4,5,6,7,8};
+    for(auto it=v1.begin();it<v1.end();it++)
+    {
+        cout<< *it<<endl;
+    }
+}
